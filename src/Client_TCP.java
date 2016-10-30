0967f01c18595b173cdbaaaa115492cb67bfd84f
Роман Зайцев < >
2016-10-30 16:33:53 +0300

    Laba finished

--- src/Client_TCP.java
+++ src/Client_TCP.java
@@ -3,70 +3,91 @@ import java.net.*;
 import java.util.Scanner;
 
 public class Client_TCP {
-    private DatagramSocket socket;
-    private String name = "client", serverName = "server";
-    private BufferedReader keyboard;
-    private Thread listener;
-    private InetAddress ipAddress;
-    private int port;
+	private Socket socket;
+	private DataInputStream in;
+	private DataOutputStream out;
+	private String name, serverName;
+	private BufferedReader keyboard;
+	private Thread listener;
 
-    public void Client(String adr, int port) throws SocketException, UnknownHostException {
-        ipAddress = InetAddress.getByName(adr);
-        this.port = port;
-        socket = new DatagramSocket();
-        keyboard = new BufferedReader(new InputStreamReader(System.in));
-        listener = new Thread(new FromServer());
-        listener.start();
-    }
+	public Client_TCP(String adr, int port) throws IOException {
+		InetAddress ipAddress = InetAddress.getByName("127.0.0.1");
+		System.out.println("Wel!");
+		socket = new Socket(InetAddress.getByName("127.0.0.1"), 17000);
+		System.out.println("Welcome!");
+		in = new DataInputStream(socket.getInputStream());
+		out = new DataOutputStream(socket.getOutputStream());
+		keyboard = new BufferedReader(new InputStreamReader(System.in));
+		listener = new Thread(new FromServer());
+		listener.start();
+	}
 
-    public static void main(String[] args) throws IOException {
-        System.out.println("Welcome!");
-        int port =17000; //Integer.parseInt(args[0]);
-        new Client("127.0.0.1"/*args[1]*/, port).run();
-    }
+	public static void main(String[] args) throws IOException {
+		System.out.println("Welcome!");
+		new Client_TCP("localhost", 17000).run();
+	}
 
-    public void run() {
-        try {
-            while (true) {
-                String line;
-                line = keyboard.readLine();
-                if (socket.isClosed())
-                    break;
-                send(line);
-                if (line.equals("@quit")) {
-                    socket.close();
-                    break;
-                }
-                if (line.contains("@name"))
-                    name = line.substring("@name".length() + 1);
-            }
-        } catch (Exception x) {
-            x.printStackTrace();
-        }
-    }
+	public void run() {
+		try {
+			while (true) {
+				String line;
+				line = keyboard.readLine();
+				if (socket.isClosed())
+					break;
+				out.writeUTF(line);
+				if (line.equals("@quit")) {
+					socket.close();
+					break;
+				}
+				if (line.equals("close")) {
+					socket.close();
+					break;
+				}
+				if (line.contains("@name"))
+					name = line.substring("@name".length() + 1);
+			}
+		} catch (Exception x) {
+			x.printStackTrace();
+		} finally {
+			try {
+				socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-    private class FromServer implements Runnable {
+	private class FromServer implements Runnable {
 
-        public void run() {
-            try {
-                while (true) {
-                    String line;
-                    line = read(); 
-                    if (line.equals("@quit")) {
-                        System.out.println("server is quited");
-                        break;
-                    }
-                    if (line.contains("@name"))
-                        serverName = line.substring("@name".length() + 1);
-                    else
-                        System.out.println(serverName + ": " + line);
-                }
-            } catch (IOException e) {
-                socket.close();
-            } finally {
-                socket.close();
-            }
-        }
-    }
+		public void run() {
+			try {
+				while (true) {
+					String line;
+					line = in.readUTF();
+					if (line.equals("@quit") || line.equals("@close")) {
+						System.out.println("server and client is quited");
+						socket.close();
+						break;
+					}
+					if (line.contains("@name"))
+						serverName = line.substring("@name".length() + 1);
+					else
+						System.out.println(serverName + ": " + line);
+				}
+			} catch (IOException e) {
+				try {
+					socket.close();
+				} catch (IOException s) {
+					s.printStackTrace();
+				}
+			} finally {
+				try {
+					socket.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
 }
--- src/Server_TCP.java
+++ src/Server_TCP.java
@@ -1,76 +1,105 @@
 import java.net.*;
 import java.io.*;
 import java.util.Scanner;
+/* logic break and continue*/
+
 
 public class Server_TCP {
-    private ServerSocket ser_socket;
-    private Socket socket;
-    private String name = "server", clientName = "client";
-    private BufferedReader keyboard;
-    private Thread sender;
-    private InetAddress ipAddress;
-    
-    public Server_TCP(int port) throws IOException {
-    	ser_socket = new ServerSocket(port);
+	private ServerSocket ser_socket;
+	private Socket socket;
+	private String name, clientName;
+	private BufferedReader keyboard;
+	private Thread sender;
+	private DataInputStream in;
+	private DataOutputStream out;
+
+	public Server_TCP(int port) throws IOException {
+		ser_socket = new ServerSocket(port);
 	}
 
-    public static void main(String[] args) throws IOException {
-        System.out.println("Welcome!");
-        new Server_TCP(Integer.parseInt(args[0])).run();
-    }
+	public static void main(String[] args) throws IOException {
+		System.out.println("Welcome!");
+		new Server_TCP(17000).run();
+	}
 
-    public void run() {
-    	boolean exit;
-        try {
-        	while(true){
-        		socket = ser_socket.accept();
-        		exit = true;
-        	}
-        	
-            while (true) {
-            	
-                if (line.equals("@quit")) {
-                    System.out.println("client is quited");
-                    break;
-                }
-                if (line.contains("@name"))
-                    clientName = line.substring("@name".length() + 1);
-                else {
-                    send(line);
-                    System.out.println(clientName + ": " + line);
-                }
-            }
-        } catch (IOException e) {
-            socket.close();
-        } finally {
-            socket.close();
-        }
-    }
+	public void run() throws IOException {
+		try {
+			while (true) {
+				socket = ser_socket.accept();
+				in = new DataInputStream(socket.getInputStream());
+				out = new DataOutputStream(socket.getOutputStream());
+				sender = new Thread(new Sender());
+				sender.start();
+				while (true) {
+					String line = in.readUTF();
+					if (line.equals("@quit")) {
+						System.out.println("client is quited");
+						socket.close();
+						break;
+					}
+					if (line.equals("@close")) {
+						System.out.println("server closed");
+						close();
+						break;
+					}
+					if (line.contains("@name"))
+						clientName = line.substring("@name".length() + 1);
+					else {
+						System.out.println(clientName + ": " + line);
+					}
+				}
+			}
+		} catch (SocketException e) {
+			close();
+		} finally {
+			close();
+		}
+	}
 
-    private class Sender implements Runnable {
+	private void close() {
+		try {
+			ser_socket.close();
+			socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-        Sender() {
-            keyboard = new BufferedReader(new InputStreamReader(System.in));
-        }
+	private class Sender implements Runnable {
 
-        public void run() {
-            try {
-                while (!socket.isClosed()) {
-                    String line;
-                    line = keyboard.readLine();
-                    if (line != null && !socket.isClosed()) {
-                        send(line);
-                        if (line.equals("@quit")) {
-                            socket.close();
-                            break;
-                        }
-                        if (line.contains("@name"))
-                            name = line.substring("@name".length() + 1);
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+		Sender() {
+			keyboard = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public void run() {
+			try {
+				while (!socket.isClosed()) {
+					String line;
+					line = keyboard.readLine();
+					if (line != null && !socket.isClosed()) {
+						out.writeUTF(line);
+						out.flush();
+						if (line.equals("@quit")) {
+							socket.close();
+							break;
+						}
+						if (line.equals("@close")) {
+							System.out.println("server closed");
+							close();
+							break;
+						}
+						if (line.contains("@name"))
+							name = line.substring("@name".length() + 1);
+
+					}
+				}
+			} catch (Exception e) {
+				if ("Socket closed".equals(e.getMessage())) {
+					close();
+				}
+				e.printStackTrace();
+				close();
+			}
+		}
+	}
 }
